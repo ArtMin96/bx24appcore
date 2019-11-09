996e45dc644d80e1b8dc17503ec2b845f8948dcc
unknown <nordeveloper@gmail.com>
2019-11-10 00:22:17 +0400

    Updated run.php

--- run.php
+++ run.php
@@ -3,4 +3,6 @@
 require __DIR__ . "/vendor/autoload.php";
 
 use Bitrix24\Crm;
-$crm = new Crm('https://kreativsites.bitrix24.ru/rest/1/5fjaudjkhr3600jo/');
+
+$webhook = '';
+$crm = new Crm($webhook);
